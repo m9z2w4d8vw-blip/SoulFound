1fb921b4ccb73f3eaaf27e3ac64b21f5f3417f16
m9z2w4d8vw-blip <m9z2w4d8vw@privaterelay.appleid.com>
2026-06-30 16:30:58 -0700

    fix: cancel previous search task before starting new search

--- SoulFound/Network/SearchManager.swift
+++ SoulFound/Network/SearchManager.swift
@@ -7,12 +7,12 @@ class SearchManager: ObservableObject {
     @Published var isSearching = false
 
     private var currentToken: UInt32?
+    private var searchTask: Task<Void, Never>?
     private var cancellable: AnyCancellable?
     private weak var client: SoulseekClient?
 
     func attach(to client: SoulseekClient) {
         self.client = client
-        // Watch the client's searchResultsByToken for our current token
         cancellable = client.$searchResultsByToken
             .receive(on: RunLoop.main)
             .sink { [weak self] allResults in
@@ -25,15 +25,23 @@ class SearchManager: ObservableObject {
 
     func search(query: String) async {
         guard let client else { return }
+
+        // Cancel any in-flight search before starting a new one
+        searchTask?.cancel()
+
         isSearching = true
         results = []
 
         let token = client.search(query: query)
         currentToken = token
 
-        // Wait 10 seconds for results to arrive from peers
-        try? await Task.sleep(nanoseconds: 10_000_000_000)
+        searchTask = Task {
+            try? await Task.sleep(nanoseconds: 10_000_000_000)
+            if !Task.isCancelled {
+                isSearching = false
+            }
+        }
 
-        isSearching = false
+        await searchTask?.value
     }
 }
